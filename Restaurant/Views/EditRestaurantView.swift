f5cf61d0c19caec66ffc1f3efa2574522e309834
Aruzhan <82173858+zhkhnn@users.noreply.github.com>
2023-04-19 00:28:53 +0600

    user defaults

--- Restaurant/Views/EditRestaurantView.swift
+++ Restaurant/Views/EditRestaurantView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct EditRestaurantView: View {
+    @ObservedObject var userPreferences = UserDefaults()
     @Environment(\.managedObjectContext) var ManagedObjectContext
     @Environment(\.dismiss) var dismiss
     var restaurant: FetchedResults<Restaurant>.Element
     @State private var name = ""
     @State private var rate: Double = 0
+    @State private var isDarkModeEnabled = false
+    
+    var backgroundColor: Color {
+            userPreferences.isDarkModeEnabled ? Color(UIColor.gray) : Color(UIColor.systemBackground)
+        }
     
     var body: some View {
         Form{
@@ -24,7 +30,14 @@ struct EditRestaurantView: View {
                 VStack{
                     Text("Rate: \(Int(rate))")
                     Slider(value: $rate, in: 0...10, step: 1)
+                    Toggle(isOn: $userPreferences.isDarkModeEnabled) {
+                                Text("Dark mode")
+                            }
                 }
+                
+//                .onChange(of: userPreferences.isDarkModeEnabled) { newValue in
+//                            self.isDarkModeEnabled = newValue
+//                        }
                 .padding()
                 HStack{
                     Spacer()
@@ -35,7 +48,23 @@ struct EditRestaurantView: View {
                     Spacer()
                 }
             }
+            .onAppear {
+                        self.isDarkModeEnabled = userPreferences.isDarkModeEnabled
+                    }
+            .background(backgroundColor.ignoresSafeArea())
         }
+    }
+}
+class UserDefaults: ObservableObject{
+    private let isDarkModeEnabledKey = "isDarkModeEnabled"
+    @Published var isDarkModeEnabled: Bool {
+        didSet {
+            Foundation.UserDefaults.standard.set(isDarkModeEnabled, forKey: isDarkModeEnabledKey)
+        }
+    }
+
+    init() {
+        self.isDarkModeEnabled = Foundation.UserDefaults.standard.bool(forKey: isDarkModeEnabledKey)
     }
 }
 
